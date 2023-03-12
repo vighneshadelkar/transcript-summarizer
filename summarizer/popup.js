0b5748a4f40188d794dfa8e05ed1d88bec6b5be6
Vighnesh Adelkar <74565079+vighneshadelkar@users.noreply.github.com>
2023-03-12 17:32:33 +0530

    added loading js

--- summarizer/popup.js
+++ summarizer/popup.js
@@ -1,7 +1,30 @@
 const btn = document.getElementById("summary");
+const txt=document.getElementById("txt");
+let loadParent=document.getElementById("loading");
+
 btn.addEventListener("click", function() {
     btn.disabled = true;
+
+    //JS to add loader to screen
+    let loader=document.createElement("div");
+    loader.setAttribute("id","lds-spinner");
+    loader.innerHTML=`<div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>`;
+    btn.style.width="100px";
+
     btn.innerHTML = "Summarising...";
+    txt.innerHTML="Your summary is getting ready..";
+    loadParent.appendChild(loader);
     console.log("I was here");
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         console.log(tabs);
@@ -13,25 +36,15 @@ btn.addEventListener("click", function() {
         fetch(noturl) 
         .then((response) => response.text())
         .then((data) => {
+            loadParent.removeChild(loader);
+            txt.innerHTML="Your summary ready..";
             console.log("I was def here");
             console.log(data);
             const p = document.getElementById("summedtxt");
             p.innerHTML = data;
             btn.disabled = false;
+            txt.innerHTML="Press Summarise to get the Summary.";
             btn.innerHTML = "Summarise";
         });
     });
 });
-
-let loader=document.getElementById("lds-spinner");
-let txt=document.getElementById("summedtxt");
-
-let txtVal=txt.innerText;
-
-console.log(txtVal);
-
-if(txtVal!="")
-{
-    loader.style.display="none";
-}
-
